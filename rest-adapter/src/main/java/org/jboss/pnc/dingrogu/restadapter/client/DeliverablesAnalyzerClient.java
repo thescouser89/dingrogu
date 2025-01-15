930f6097b288a7249edb8ec827aee5bd27c7b497
Dustin Kut Moy Cheung <dcheung@redhat.com>
2025-01-14 20:38:43 -0500

    Add debug lines for dela client

--- rest-adapter/src/main/java/org/jboss/pnc/dingrogu/restadapter/client/DeliverablesAnalyzerClient.java
+++ rest-adapter/src/main/java/org/jboss/pnc/dingrogu/restadapter/client/DeliverablesAnalyzerClient.java
@@ -1,5 +1,6 @@
 package org.jboss.pnc.dingrogu.restadapter.client;
 
+import io.quarkus.logging.Log;
 import io.quarkus.oidc.client.Tokens;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -24,6 +25,9 @@ public class DeliverablesAnalyzerClient {
                 .asJson();
 
         if (!response.isSuccess()) {
+            Log.info(response.getStatus());
+            Log.info(response.getStatusText());
+            Log.info(response.getBody().toPrettyString());
             throw new RuntimeException("Request didn't go through");
         }
     }
