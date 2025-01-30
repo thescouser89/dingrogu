3cd3d41c43f8919f30848e848a8ed7e565232667
Dustin Kut Moy Cheung <dcheung@redhat.com>
2025-01-30 17:15:56 -0500

    Update RepositoryCreationDTO fields

--- api/src/main/java/org/jboss/pnc/dingrogu/api/dto/workflow/RepositoryCreationDTO.java
+++ api/src/main/java/org/jboss/pnc/dingrogu/api/dto/workflow/RepositoryCreationDTO.java
@@ -2,13 +2,18 @@ package org.jboss.pnc.dingrogu.api.dto.workflow;
 
 import lombok.Builder;
 import lombok.Data;
+import org.jboss.pnc.api.enums.JobNotificationType;
 
 @Data
 @Builder
 public class RepositoryCreationDTO {
 
+    public String orchUrl;
     public String repourUrl;
+
     public String externalRepoUrl;
     public String ref;
+    public boolean preBuildSyncEnabled;
+    public JobNotificationType jobNotificationType;
 
 }
