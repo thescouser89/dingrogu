2d5475b25c0ba380cea0731c0f224d85962c2b56
Dustin Kut Moy Cheung <dcheung@redhat.com>
2025-01-31 13:10:02 -0500

    Add tests for common module

---
+++ common/src/test/java/org/jboss/pnc/dingrogu/common/GitUrlParserTest.java
@@ -0,0 +1,33 @@
+package org.jboss.pnc.dingrogu.common;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GitUrlParserTest {
+
+    @Test
+    void generateInternalGitRepoName() {
+        String gitRepository = "https://github.com/project-ncl/dingrogu";
+        assertThat(GitUrlParser.generateInternalGitRepoName(gitRepository)).isEqualTo("project-ncl/dingrogu");
+
+        String gitRepository2 = "https://github.com/project-ncl/dingrogu.git";
+        assertThat(GitUrlParser.generateInternalGitRepoName(gitRepository2)).isEqualTo("project-ncl/dingrogu");
+
+        String badGitRepository = "hellohub";
+        assertThat(GitUrlParser.generateInternalGitRepoName(badGitRepository)).isNull();
+    }
+
+    @Test
+    void scmRepoURLReadOnly() {
+        String readwriteUrlGitlab = "git@gitlab.com:project-ncl/bpm.git";
+        String readonlyUrlGitlab = "https://gitlab.com/project-ncl/bpm.git";
+        assertThat(GitUrlParser.scmRepoURLReadOnly(readwriteUrlGitlab)).isEqualTo(readonlyUrlGitlab);
+
+        String badReadwriteUrlGitlab = "safdltgitlacomproject-ncl";
+        assertThat(GitUrlParser.scmRepoURLReadOnly(badReadwriteUrlGitlab)).isNull();
+    }
+}
