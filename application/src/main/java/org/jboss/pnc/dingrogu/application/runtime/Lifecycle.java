daec8b6fb7f842238f2a82cd7246ddb71f248d22
Petr Kočandrle <pkocandr@redhat.com>
2025-04-11 16:07:12 +0200

    [NCL-7743] An attempt to fix conversion of Instant to json using Unirest

--- application/src/main/java/org/jboss/pnc/dingrogu/application/runtime/Lifecycle.java
+++ application/src/main/java/org/jboss/pnc/dingrogu/application/runtime/Lifecycle.java
@@ -1,22 +1,23 @@
 package org.jboss.pnc.dingrogu.application.runtime;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import io.quarkus.logging.Log;
 import io.quarkus.runtime.Shutdown;
 import io.quarkus.runtime.Startup;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import kong.unirest.core.Unirest;
 import kong.unirest.modules.jackson.JacksonObjectMapper;
 
 @ApplicationScoped
 public class Lifecycle {
 
+    @Inject
+    ObjectMapper objectMapper;
+
     @Startup
     void init() {
         // to support Optional type
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new Jdk8Module());
         Unirest.config().setObjectMapper(new JacksonObjectMapper(objectMapper));
         Log.info("Dingrogu started");
     }
