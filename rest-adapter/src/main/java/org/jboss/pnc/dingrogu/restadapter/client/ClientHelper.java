c6fd12971f560a03c42cee2fba7a5b5b2761e051
Dustin Kut Moy Cheung <dcheung@redhat.com>
2025-05-13 08:56:55 -0400

    log-client-header

--- rest-adapter/src/main/java/org/jboss/pnc/dingrogu/restadapter/client/ClientHelper.java
+++ rest-adapter/src/main/java/org/jboss/pnc/dingrogu/restadapter/client/ClientHelper.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.jboss.pnc.common.log.MDCUtils;
 
+import io.quarkus.logging.Log;
 import io.quarkus.oidc.client.Tokens;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,6 +21,10 @@ public class ClientHelper {
     public static Map<String, String> getClientHeaders(Tokens tokens) {
 
         Map<String, String> headers = MDCUtils.getHeadersFromMDC();
+        Log.info("Logging client header");
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            Log.infof("Header: %s :: Value: %s", entry.getKey(), entry.getValue());
+        }
         headers.put(AUTHORIZATION, "Bearer " + tokens.getAccessToken());
         return headers;
     }
