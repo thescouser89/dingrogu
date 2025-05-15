7c1c274a862c7fcb31e3e3cc22244d0c8d2c0c33
Dustin Kut Moy Cheung <dcheung@redhat.com>
2025-05-15 10:31:18 -0400

    AdjustReqour shouldn't retry itself

--- rest-workflow/src/main/java/org/jboss/pnc/dingrogu/restworkflow/workflows/BuildWorkflow.java
+++ rest-workflow/src/main/java/org/jboss/pnc/dingrogu/restworkflow/workflows/BuildWorkflow.java
@@ -133,7 +133,7 @@ public class BuildWorkflow implements Workflow<BuildWorkDTO> {
 
         try {
             CreateTaskDTO taskAdjustReqour = reqourAdjustAdapter
-                    .generateRexTaskRetryItself(
+                    .generateRexTask(
                             ownUrl,
                             buildWorkDTO.getCorrelationId(),
                             startRequest,
