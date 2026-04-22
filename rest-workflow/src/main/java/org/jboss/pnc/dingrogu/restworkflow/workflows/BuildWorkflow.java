b2fc5b316d35b7516af1e9d014c98b5d8d29decc
jmichalo <jmichalo@redhat.com>
2026-04-22 15:07:34 +0200

    [NCL-9557] change to sha256 for Log Attachments

--- rest-workflow/src/main/java/org/jboss/pnc/dingrogu/restworkflow/workflows/BuildWorkflow.java
+++ rest-workflow/src/main/java/org/jboss/pnc/dingrogu/restworkflow/workflows/BuildWorkflow.java
@@ -841,14 +841,12 @@ public class BuildWorkflow implements Workflow<BuildWorkDTO> {
                 return new TaskResponse<>(attachments, e.getMessage());
             }
 
-            checksums.ifPresent(
-                    (check) -> attachments.add(
-                            Attachment.builder()
-                                    .name(fromTag(tag))
-                                    .url(bifrostClient.generateGetFinalLogsURL(bifrostUrl, buildId, tag).toString())
-                                    .md5(check.getMd5())
-                                    .type(AttachmentType.LOG)
-                                    .build()));
+            checksums.ifPresent((check) -> attachments.add(Attachment.builder()
+                    .name(fromTag(tag))
+                    .url(bifrostClient.generateGetFinalLogsURL(bifrostUrl, buildId, tag).toString())
+                    .sha256(check.getSha256())
+                    .type(AttachmentType.LOG)
+                    .build()));
         }
 
         return new TaskResponse<>(attachments.isEmpty() ? null : attachments, null);
